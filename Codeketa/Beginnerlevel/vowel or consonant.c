7b206a255b05ae3db91a7f8ce6054bd20a2859e1
gunali039 <34646854+gunali039@users.noreply.github.com>
2017-12-19 12:39:01 +0530

    Update vowel or consonant.c

--- Codeketa/Beginnerlevel/vowel or consonant.c
+++ Codeketa/Beginnerlevel/vowel or consonant.c
@@ -3,7 +3,7 @@
 int main(void) {
 	char c;
 	printf("Enter the character\n");
-	scanf("%c",c);
+	scanf("%c",&c);
 	if((c=='a'||c=='A')||(c=='e'||c=='E')||(c=='i'||c=='I')||(c=='o'||c=='O')||(c=='u'||c=='U'))
 	{
 		printf("Vowel\n");
