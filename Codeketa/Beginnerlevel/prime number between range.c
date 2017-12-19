d0ab26a93d6687f9af8090beaa33801f5430ef0a
gunali039 <34646854+gunali039@users.noreply.github.com>
2017-12-19 06:03:54 -0800

    Create prime number between range.c

---
+++ Codeketa/Beginnerlevel/prime number between range.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b, i, flag;
+    printf("Enter the range\n ");
+    scanf("%d %d", &a, &b);
+
+    while (a < b)
+    {
+        flag = 0;
+
+        for(i = 2; i <= a/2; ++i)
+        {
+            if(a % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            printf("%d\n",a);
+
+        a++;
+    }
+
+	return 0;
+}
