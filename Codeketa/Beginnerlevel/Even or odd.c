e9f6ff8dde8f2005b6c4156a449a08d4b83a282d
gunali039 <34646854+gunali039@users.noreply.github.com>
2017-12-19 12:14:26 +0530

    Create Even or odd.c

---
+++ Codeketa/Beginnerlevel/Even or odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+int a;
+printf("enter the value of a\n");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("Even\n");
+}
+else
+{
+printf("Odd\n");
+}
+	
+	return 0;
+}
