24415f4150a1d27e507f49edbb19cf10605200f0
gunali039 <34646854+gunali039@users.noreply.github.com>
2017-12-19 05:26:24 -0800

    Create even numbers in range.c

---
+++ Codeketa/Beginnerlevel/even numbers in range.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int i,a,b;
+	printf("Enter the starting range\n");
+	scanf("%d",&a);
+	printf("Enter the ending range\n");
+	scanf("%d",&b);
+	for(i=a;i<=b;i++)
+	{
+		if(i%2==0)
+		{
+			printf("%d\n",i);
+		}
+	}
+	return 0;
+}
